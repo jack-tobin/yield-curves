3b83b8e3be2ae2e977fbbd7e52965f2bf0a4269b
Jack Tobin <tobjack330@gmail.com>
2025-06-17 22:41:37 +0100

    formatting

--- src/apps/yield_curves/migrations/0010_remove_bondmetric_isin_alter_bondmetric_pk.py
+++ src/apps/yield_curves/migrations/0010_remove_bondmetric_isin_alter_bondmetric_pk.py
@@ -4,19 +4,20 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('yield_curves', '0009_remove_analysis_is_deleted'),
+        ("yield_curves", "0009_remove_analysis_is_deleted"),
     ]
 
     operations = [
         migrations.RemoveField(
-            model_name='bondmetric',
-            name='isin',
+            model_name="bondmetric",
+            name="isin",
         ),
         migrations.AlterField(
-            model_name='bondmetric',
-            name='pk',
-            field=models.CompositePrimaryKey('date', 'bond_id', blank=True, editable=False, primary_key=True, serialize=False),
+            model_name="bondmetric",
+            name="pk",
+            field=models.CompositePrimaryKey(
+                "date", "bond_id", blank=True, editable=False, primary_key=True, serialize=False
+            ),
         ),
     ]
